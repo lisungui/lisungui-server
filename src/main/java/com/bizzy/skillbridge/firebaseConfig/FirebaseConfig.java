84cb968dd3d030a781189504ef1278ea7d5c5bd9
rogerjeasy <rogerjeasy@gmail.com>
2024-05-28 13:37:55 +0200

    ci file

--- src/main/java/com/bizzy/skillbridge/firebaseConfig/FirebaseConfig.java
+++ src/main/java/com/bizzy/skillbridge/firebaseConfig/FirebaseConfig.java
@@ -1,20 +1,17 @@
 package com.bizzy.skillbridge.firebaseConfig;
 
-import com.bizzy.skillbridge.configKey.ConfigKey;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
-
+import io.github.cdimascio.dotenv.Dotenv;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
 
 
 @Configuration
@@ -22,14 +19,12 @@ public class FirebaseConfig {
     
     @Bean
     public Firestore firestore() throws IOException {
-        String apiKey = ConfigKey.getApiKey();
-
-        if (apiKey == null || apiKey.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "API key not found. Value: " + apiKey);
-        }
-        FileInputStream serviceAccount = new FileInputStream(apiKey);
         
+        Dotenv dotenv = Dotenv.load();
+        String firebaseConfigPath = dotenv.get("FIRESTORE_APPLICATION_CREDENTIALS");
 
+        InputStream serviceAccount = new FileInputStream(firebaseConfigPath);
+        
         FirebaseOptions options = new FirebaseOptions.Builder()
             .setCredentials(GoogleCredentials.fromStream(serviceAccount))
             .build();
