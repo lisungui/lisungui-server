1c10e5fa251aa32a734ebb8bf3d85422617cbec1
rogerjeasy <rogerjeasy@gmail.com>
2024-05-28 16:23:53 +0200

    webconfig

--- src/main/java/com/bizzy/skillbridge/webMvcConfig/WebConfig.java
+++ src/main/java/com/bizzy/skillbridge/webMvcConfig/WebConfig.java
@@ -2,29 +2,23 @@ package com.bizzy.skillbridge.webMvcConfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
     @Bean
-    public CorsFilter corsFilter() {
-        CorsConfigurationSource source = corsConfigurationSource();
-        return new CorsFilter(source);
-    }
-
-    private CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("http://lisungui-client.s3-website.eu-north-1.amazonaws.com/"); // Frontend URL
-        configuration.addAllowedMethod("*");
-        configuration.addAllowedHeader("*");
-        configuration.setAllowCredentials(true);
-        
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://lisungui-client.s3-website.eu-north-1.amazonaws.com/")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
