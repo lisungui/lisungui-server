a24b3454e0a5766d2ef99c10dc283f53daa91141
rogerjeasy <rogerjeasy@gmail.com>
2024-07-17 02:55:56 +0200

    updated frontend allowed origin url

--- src/main/java/com/bizzy/skillbridge/webMvcConfig/WebConfig.java
+++ src/main/java/com/bizzy/skillbridge/webMvcConfig/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         // .allowedOrigins("http://lisungui-client.s3-website.eu-north-1.amazonaws.com/")
-                        .allowedOrigins("https://lisungui.onrender.com")
+                        .allowedOrigins("https://lisungui.onrender.com/")
                         // .allowedOrigins("http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
