b2eeb41d124900b8ae56fe36cd14f4f03e9dfaa4
rogerjeasy <rogerjeasy@gmail.com>
2024-07-21 19:03:16 +0200

    updated yml workflow file

--- write_api_key.sh
+++ write_api_key.sh
@@ -2,7 +2,7 @@
 
 # This script writes the API key to the app.yaml file in a specific format.
 
-# Check if the API_KEY environment variable is set
+# Check if the FIRESTORE_APPLICATION_CREDENTIALS_JSON environment variable is set
 if [ -z "$FIRESTORE_APPLICATION_CREDENTIALS_JSON" ]; then
   echo "ERROR: FIRESTORE_APPLICATION_CREDENTIALS_JSON environment variable is not set."
   exit 1
@@ -18,9 +18,15 @@ if [ ! -f "$APP_YAML_PATH" ]; then
 fi
 
 # Prepare the content to be written
-CONTENT="env_variables:\n  ENV: 'prod'\n  FIRESTORE_APPLICATION_CREDENTIALS_JSON: '$FIRESTORE_APPLICATION_CREDENTIALS_JSON'"
+CONTENT="\nenv_variables:\n  ENV: 'prod'\n  FIRESTORE_APPLICATION_CREDENTIALS_JSON: '$FIRESTORE_APPLICATION_CREDENTIALS_JSON'"
 
-# Write the content to the app.yaml
-echo "$CONTENT" >> $APP_YAML_PATH
+# Write the content to the app.yaml, ensuring proper formatting
+if grep -q "env_variables:" "$APP_YAML_PATH"; then
+  echo "Updating existing env_variables section."
+  sed -i "/env_variables:/a \  ENV: 'prod'\n  FIRESTORE_APPLICATION_CREDENTIALS_JSON: '$FIRESTORE_APPLICATION_CREDENTIALS_JSON'" "$APP_YAML_PATH"
+else
+  echo "Adding new env_variables section."
+  echo -e "$CONTENT" >> "$APP_YAML_PATH"
+fi
 
 echo "API key has been written to $APP_YAML_PATH"
