50e3b36aef1fbd9b9355d2ca2e0e75ff26bce8a7
rogerjeasy <rogerjeasy@gmail.com>
2024-07-22 18:39:24 +0200

    allowed three origins

--- src/main/java/com/bizzy/skillbridge/webMvcConfig/WebConfig.java
+++ src/main/java/com/bizzy/skillbridge/webMvcConfig/WebConfig.java
@@ -14,10 +14,10 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*"
-                                // "https://lemon-smoke-0d0925a0f.5.azurestaticapps.net",
+                        .allowedOrigins("http://lisungui-client.s3-website.eu-north-1.amazonaws.com",
+                                "https://lemon-smoke-0d0925a0f.5.azurestaticapps.net",
                                 // "http://localhost:3000",
-                                // "https://lisungui.onrender.com"
+                                "https://lisungui.onrender.com"
                                 )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
