a47a3528dca1606bcb183fa70e9be2511f99f386
rogerjeasy <rogerjeasy@gmail.com>
2024-07-22 11:38:41 +0200

    updated firebase config

--- src/main/java/com/bizzy/skillbridge/firebaseConfig/FirebaseConfig.java
+++ src/main/java/com/bizzy/skillbridge/firebaseConfig/FirebaseConfig.java
@@ -9,29 +9,39 @@ import io.github.cdimascio.dotenv.Dotenv;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration
 public class FirebaseConfig {
-    
+
     @Bean
     public Firestore firestore() throws IOException {
-        
-        Dotenv dotenv = Dotenv.load();
-        String firebaseConfigPath = dotenv.get("FIRESTORE_APPLICATION_CREDENTIALS");
 
-        InputStream serviceAccount = new FileInputStream(firebaseConfigPath);
-        
-        FirebaseOptions options = new FirebaseOptions.Builder()
-            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-            .build();
+        FirebaseOptions options;
 
-            if (FirebaseApp.getApps().isEmpty()) { // check with existing apps are already initialized
-                FirebaseApp.initializeApp(options);
-                }
+        // Check if running locally by looking for an environment variable or system property
+        String environment = System.getenv("ENVIRONMENT");
+        if (environment == null || environment.equals("LOCAL")) {
+            // Load credentials from Dotenv for local development
+            Dotenv dotenv = Dotenv.load();
+            String firebaseConfigPath = dotenv.get("FIRESTORE_APPLICATION_CREDENTIALS");
+            InputStream serviceAccount = new FileInputStream(firebaseConfigPath);
+
+            options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+        } else {
+            // Use Application Default Credentials for GCP
+            options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.getApplicationDefault())
+                .build();
+        }
+
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseApp.initializeApp(options);
+        }
+
         return FirestoreClient.getFirestore();
     }
 }
