ee3120674e10682d54bb6ab49c0f3302f8f3565f
rogerjeasy <rogerjeasy@gmail.com>
2024-07-22 15:10:41 +0200

    updated firebase config

--- src/main/java/com/bizzy/skillbridge/firebaseConfig/FirebaseConfig.java
+++ src/main/java/com/bizzy/skillbridge/firebaseConfig/FirebaseConfig.java
@@ -21,22 +21,22 @@ public class FirebaseConfig {
         FirebaseOptions options;
 
         // Check if running locally by looking for an environment variable or system property
-        String environment = System.getenv("ENVIRONMENT");
-        if (environment == null || environment.equals("LOCAL")) {
-            // Load credentials from Dotenv for local development
-            Dotenv dotenv = Dotenv.load();
-            String firebaseConfigPath = dotenv.get("FIRESTORE_APPLICATION_CREDENTIALS");
-            InputStream serviceAccount = new FileInputStream(firebaseConfigPath);
+        // String environment = System.getenv("ENVIRONMENT");
+        // if (environment == null || environment.equals("LOCAL")) {
+        //     // Load credentials from Dotenv for local development
+        //     Dotenv dotenv = Dotenv.load();
+        //     String firebaseConfigPath = dotenv.get("FIRESTORE_APPLICATION_CREDENTIALS");
+        //     InputStream serviceAccount = new FileInputStream(firebaseConfigPath);
 
-            options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .build();
-        } else {
+        //     options = new FirebaseOptions.Builder()
+        //         .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+        //         .build();
+        // } else {
             // Use Application Default Credentials for GCP
             options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.getApplicationDefault())
                 .build();
-        }
+        // }
 
         if (FirebaseApp.getApps().isEmpty()) {
             FirebaseApp.initializeApp(options);
