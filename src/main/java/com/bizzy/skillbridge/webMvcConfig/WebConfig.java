096a2542097f5a38cc4a5d49780762481a4700f2
rogerjeasy <rogerjeasy@gmail.com>
2024-07-22 16:11:07 +0200

    added / to frontend url origin

--- src/main/java/com/bizzy/skillbridge/webMvcConfig/WebConfig.java
+++ src/main/java/com/bizzy/skillbridge/webMvcConfig/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://lisungui-client.s3-website.eu-north-1.amazonaws.com"
+                        .allowedOrigins("http://lisungui-client.s3-website.eu-north-1.amazonaws.com/"
                                 // "https://lemon-smoke-0d0925a0f.5.azurestaticapps.net",
                                 // // "http://localhost:3000",
                                 // "https://lisungui.onrender.com"
